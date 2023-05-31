e684ba36647f2d511be53a5bfb5e9c646d746698
Lydia <pricelydia776@gmail.com>
2023-05-31 20:38:39 +1200

    Base Formatting Complete

--- BookingSystem/static/cancel_booking.js
+++ BookingSystem/static/cancel_booking.js
@@ -3,7 +3,7 @@ function display_outcome(outcome) {
     if (outcome === 'success') {
         outcome_div.innerHTML += 'Your booking was cancelled successfully'
     } else {
-        outcome_div.innerHTML += 'There was an error cancelling your booking. ' +
-            'Please try again later or contact out help desk during business hours '
+        outcome_div.innerHTML += 'There was an error cancelling your booking <br>' +
+            'Please try again later or contact our help desk during business hours '
     }
 }
