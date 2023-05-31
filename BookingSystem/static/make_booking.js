b7c637f1948135c9b240c5d05e41d7e9918da387
Lydia <pricelydia776@gmail.com>
2023-05-31 19:42:26 +1200

    Make Booking Styling complete

--- BookingSystem/static/make_booking.js
+++ BookingSystem/static/make_booking.js
@@ -1,6 +1,11 @@
-function display_flight(flight, route) {
+function display_flight(flight, route, airports) {
+    let heading_div = document.getElementById("heading");
+    heading_div.innerHTML += `From ${airports['departure']} to ${airports['arrival']}`;
+    document.getElementById('flight_details_header').innerHTML += `<h3>Flight Details:</h3>`
+
+    document.getElementById('passenger_details').innerHTML += `<h3>Your Details:</h3>`
+
     let flight_data = display_flight_data(flight, route, "flight_to_book")
-    let form = document.getElementById('booking_details');
     let booking_button = document.getElementById('book_button');
     console.log(flight['seats_available'])
     if (flight['seats_available'] > 0) {
@@ -8,24 +13,25 @@ function display_flight(flight, route) {
         booking_button.title = '';
     } else {
         booking_button.disabled = true;
-        booking_button.title = "No seats left on this flight";
-
     }
 
-    form.insertBefore(flight_data, form.firstChild);
+    document.getElementById("flight_details").appendChild(flight_data);
 }
 
 function display_flight_data(flight, route, id) {
     let flight_data = document.createElement('div');
     flight_data.id = id;
-    flight_data.innerHTML += `Date: ${flight['date']} <br>` +
-        `Flight Number: ${flight['route_id']} <br>` +
-        `Price: $${flight['price']} NZD<br>` +
-        `Departure Time: ${route['departure_time']} <br>` +
-        `Arrival Time: ${route['arrival_time']} <br>`;
+    flight_data.innerHTML += `<b>Date:</b> ${flight['date']} <br>` +
+        `<b>Flight Number</b>: ${flight['route_id']} <br>` +
+        `<b>Price:</b> $${flight['price']} NZD<br>` +
+        `<b>Departure Time:</b> ${route['departure_time']} <br>` +
+        `<b>Arrival Time:</b> ${route['arrival_time']} <br>`;
     if (route['stopover_location_id'] != null) {
         flight_data.innerHTML += `Stopover Location: ${route['stopover_location_id']} <br>` +
-            `Stopover Length: ${route['stopover_time']} <br>`;
+            `Stopover Length: ${route['stopover_time']} minutes<br>`;
+    } else {
+        flight_data.innerHTML += `<b>Stopover Location:</b> NA <br>` +
+            `<b>Stopover Length:</b> NA <br>`;
     }
     return flight_data;
 }
